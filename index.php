6f5089d817057d236c687b40afd048f13def5d0c
a-sajadi <arefsajjadi80@gmail.com>
2021-07-27 18:42:18 +0430

    add index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>simpleRecaptcha</title>
+</head>
+
+<body>
+<main>
+    <!-- show security image -->
+    <img src="reCAPTCHA-builder.php" alt="recaptcha"><br>
+    <!-- get security code -->
+    <input type="text" title="code" onkeyup="send();" id="code" maxlength="6"/>
+    <!-- show result -->
+    <div id="result"></div>
+</main>
+</body>
+
+<script src="vendor/jquery/jquery.js"></script>
+<script>
+    // ajax
+    function send() {
+        $.post('controller.php', {
+                code: $("#code").val(),
+            },
+            function (data) {
+                $('#result').html(data);
+            });
+    }
+</script>
+</html>
